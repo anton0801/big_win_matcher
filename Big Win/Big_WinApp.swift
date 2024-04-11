283cbd2e5cc4278a26273bca7480feba4255f858
Anton <danilovanton724@gmail.com>
2024-04-11 20:41:34 +0200

    Initial Commit

---
+++ Big Win/Big_WinApp.swift
@@ -0,0 +1,17 @@
+//
+//  Big_WinApp.swift
+//  Big Win
+//
+//  Created by Anton on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Big_WinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
